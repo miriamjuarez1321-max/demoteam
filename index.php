4ab3697ff86b4e9c6fd3ff72d58651cb27687f0e
yuyisanchezalv2006-stack <yuyisanchezalv2006@gmail.com>
2026-01-29 12:25:58 -0600

    Add initial index.php file with team introduction and member links

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>bienvenidos</h1>
+    <br>
+    <h2>Equipo MoveOnSport</h2>
+    <br>
+    <h2>Integrantes:</h2>
+    <ul>
+        <li><a href="./oliver.docx">Oliver Elnivar</a></li>
+        <li><a href="./miriam.docx">Miriam Noemi</a></li>
+        <li><a href="./jose.docx">Jose Alfredo</a></li>
+        <li><a href="./jordy.docx">Jordy Rafael</a></li>
+         <li><a href="./oliverdavid.docx">Oliver David</a></li>
+    </ul>
+
+
+</body>
+</html>
